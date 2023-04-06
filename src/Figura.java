069facc77a090eb650bc9b0b12750a9fd8f049b7
leojazz <leonardosousa.linux@gmail.com>
2023-04-06 14:28:16 -0300

    Execicio Poo Ultima Proff Rafa

---
+++ src/Figura.java
@@ -0,0 +1,15 @@
+public abstract class Figura {
+
+    private String cor;
+
+
+    public String getCor() {
+        return cor;
+    }
+     
+    public void setCor(String cor)  {
+        this.cor = cor;
+        
+    }   
+
+}
